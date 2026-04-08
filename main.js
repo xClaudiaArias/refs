b843943453fb1d9bbef19f333998a73f0f5807cd
xClaudiaArias <xClaudiaArias@gmail.com>
2026-04-08 10:19:36 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// control app window 
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 900,
+        height: 600,
+        backgroundColor: "#fff",
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
